9f4260b93846953ab6d7762699c4636819556e6d
zafuzi <zacharyfoutz@gmail.com>
2016-10-18 21:09:50 -0700

    Cleaned up sources view and remove Owin Provider, they were uneeded and causing errors.

--- Newly Read/Models/IdentityModel.cs
+++ Newly Read/Models/IdentityModel.cs
@@ -5,18 +5,12 @@ using System.Web;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity;
 
-namespace Newly_Read.Models
-{
-    public class IdentityModel
-    {
+namespace Newly_Read.Models {
+    public class IdentityModel {
         public class ApplicationUser : IdentityUser {
-            internal string email;
-            internal string firstName;
         }
-        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-        {
-            public ApplicationDbContext() : base("DefaultConnection")
-            {
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+            public ApplicationDbContext() : base("DefaultConnection") {
             }
         }
     }
